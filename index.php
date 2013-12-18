f934a171e73093515e6bcae8e04895bac4cd37b1
thebra <jason.bradley@gmail.com>
2013-12-18 10:44:00 -0600

    First Commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="vendor/twitter/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+        </ul>
+        <h3 class="text-muted">Pretty Cron</h3>
+      </div>
+
+        <br/>
+
+      <div class="jumbotron">
+        <h1>Visualize Your Cron</h1>
+        <p class="lead">Paste your crontab and hit visualize.</p>
+
+        <textarea style="height:200px;" class="form-control"></textarea>
+        <br/>
+        <p style="float:right"><a class="btn btn-lg btn-success" href="#" role="button">Visualize</a></p>
+        <br clear="all">
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="vendor/components/jquery/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="vendor/twitter/bootstrap/dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
